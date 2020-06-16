1f110dd043ae5cec195acdf3be814db91d8084fb
Chris Carr <krzysztof@Chriss-MacBook-Pro.local>
2020-06-16 14:07:48 +0200

    moced directory and shop data into its own reducer

--- crwn-clothing/src/components/collection-overview/collection-overview.component.jsx
+++ crwn-clothing/src/components/collection-overview/collection-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import "./collection-overview.style.scss";
 
 const CollectionOverview = ({ collections }) => (
@@ -16,7 +16,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
